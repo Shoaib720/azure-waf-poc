496a8a8d4f99fd75860647995cc07bfc09d8b5c8
Shoaib Shaikh <shoaibsshaikh1999@gmail.com>
2024-10-27 09:55:27 +0530

    Add agw

--- infra/main.tf
+++ infra/main.tf
@@ -19,23 +19,107 @@ resource "azurerm_resource_group" "azure_rg" {
   }
 }
 
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = "vnet-${var.project}"
-#   location            = local.location
-#   resource_group_name = azurerm_resource_group.azure_rg.name
-#   address_space       = ["10.0.0.0/16"]
-#   dns_servers         = ["10.0.0.4", "10.0.0.5"]
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${var.project}"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.azure_rg.name
+  address_space       = ["10.0.0.0/16"]
 
-#   subnet {
-#     name             = "snet-${var.project}"
-#     address_prefixes = ["10.0.1.0/24"]
-#     # security_group   = azurerm_network_security_group.example.id
-#   }
+  tags = {
+    environment = local.env
+  }
+}
 
-#   tags = {
-#     environment = local.env
-#   }
-# }
+resource "azurerm_subnet" "agw_snet" {
+  name                 = "snet-${var.project}-agw"
+  resource_group_name  = azurerm_resource_group.azure_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "workloads_snet" {
+  name                 = "snet-${var.project}-workloads"
+  resource_group_name  = azurerm_resource_group.azure_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "agw_pip" {
+  name                = "pip-${var.project}"
+  resource_group_name = azurerm_resource_group.azure_rg.name
+  location            = local.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = local.env
+  }
+}
+
+resource "azurerm_application_gateway" "network" {
+  name                = "agw-${var.project}"
+  resource_group_name = azurerm_resource_group.azure_rg.name
+  location            = local.location
+
+  sku {
+    name     = "WAF_v2"
+    tier     = "WAF_v2"
+    capacity = 1
+  }
+
+  gateway_ip_configuration {
+    name      = "gwipconf-${var.project}"
+    subnet_id = azurerm_subnet.agw_snet.id
+  }
+
+  frontend_port {
+    name = "${var.project}-fp"
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = "${var.project}-fipconf"
+    public_ip_address_id = azurerm_public_ip.agw_pip.id
+  }
+
+  backend_address_pool {
+    name = "${var.project}-bepool"
+    fqdns = [ azurerm_container_app.app.latest_revision_fqdn ]
+  }
+
+  backend_http_settings {
+    name                  = "${var.project}-besettings"
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = "${var.project}-listener"
+    frontend_ip_configuration_name = "${var.project}-fipconf"
+    frontend_port_name             = "${var.project}-fp"
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = "${var.project}-rtrule"
+    priority                   = 9
+    rule_type                  = "Basic"
+    http_listener_name         = "${var.project}-listener"
+    backend_address_pool_name  = "${var.project}-bepool"
+    backend_http_settings_name = "${var.project}-besettings"
+  }
+
+  waf_configuration {
+    enabled                  = true
+    firewall_mode            = "Detection"
+    rule_set_type            = "OWASP"
+    rule_set_version         = "3.2"
+    file_upload_limit_mb     = 100
+    request_body_check       = true
+  }
+
+}
 
 resource "azurerm_log_analytics_workspace" "log_workspace" {
   name                = "law-${var.project}"
